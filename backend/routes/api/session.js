7b4c1eb770205bed008703b637ac04592d46c072
JustineJang <justinejang415@gmail.com>
2022-07-25 00:24:23 -0700

    session

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,11 +1,8 @@
 const express = require('express');
-
-const { Booking, Image, Review, Spot, User } = require('../../db/models');
-
+const { User } = require('../../db/models');
 const { check } = require("express-validator");
 const { handleValidationErrors } = require("../../utils/validation");
-const { setTokenCookie, requireAuth, restoreUser } = require("../../utils/auth");
-
+const { setTokenCookie, restoreUser } = require("../../utils/auth");
 const router = express.Router();
 
 const validateLogin = [
@@ -19,49 +16,51 @@ const validateLogin = [
   handleValidationErrors
 ];
 
-// Log In a User
-router.post( '/login', validateLogin, async (req, res, next) => {
-  const { credential, password } = req.body;
+// Restore session user
+router.get('/', restoreUser, ( req, res ) => {
+  const { user } = req;
+  
+  if ( user ) {
+    return res.json({
+      user: user.toSafeObject()
+    });
+  } else return res.json({});
 
+});
+
+// Log In a User
+router.post('/login', validateLogin, async ( req, res, next ) => {
+  const { credential, password } = req.body;
   const user = await User.login({ credential, password });
 
-  if (!user) {
-    const err = new Error('Authentication required');
+  if ( !user ) {
+    const err = new Error('Login failed');
     err.status = 401;
-    err.title = 'Authentication required';
-    err.errors = ['Authentication required'];
+    err.title = 'Login failed';
+    err.errors = ['The provided credentials were invalid.'];
     return next(err);
   }
 
-  await setTokenCookie(res, user);
+  const token = await setTokenCookie(res, user);
+
+  user.save();
 
   return res.json({
-    user
+    "id": user.id,
+    "firstName": user.firstName,
+    "lastName": user.lastName,
+    "email": user.email,
+    "token": token
   });
 
 });
 
 // Log out
-router.delete(
-    '/',
-    (_req, res) => {
-      res.clearCookie('token');
-      return res.json({ message: 'success' });
-    }
-  );
+router.delete('/', ( _req, res ) => {
+  res.clearCookie('token');
 
-// Restore session user
-router.get(
-    '/',
-    restoreUser,
-    (req, res) => {
-      const { user } = req;
-      if (user) {
-        return res.json({
-          user: user.toSafeObject()
-        });
-      } else return res.json({});
-    }
-  );
+  return res.json({ message: 'success' });
+
+});
   
 module.exports = router;
