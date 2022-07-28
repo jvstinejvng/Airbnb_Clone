23e1493a261a5c5d549d1763c70222b7f55efa4f
JustineJang <justinejang415@gmail.com>
2022-07-27 18:56:53 -0700

    create spot

--- frontend/src/components/CreateSpotPage/index.js
+++ frontend/src/components/CreateSpotPage/index.js
@@ -1,169 +1,189 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Redirect, useHistory } from "react-router-dom";
-import { createSpot } from "../../store/spots";
+import { useHistory } from "react-router-dom";
+import * as spotActions from '../../store/spots'
 import "./CreateSpot.css";
 
-const CreateNewSpot = () => {
-  const history = useHistory();
+const CreateSpotPage = () => {
+
   const dispatch = useDispatch();
-  const sessionUser = useSelector((state) => state.session.user);
-  const [address, setAddress] = useState("");
-  const [city, setCity] = useState("");
-  const [state, setState] = useState("");
-  const [country, setCountry] = useState("");
-  const [lat, setLat] = useState("");
-  const [lng, setLng] = useState("");
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
-  const [previewImage, setPreviewImage] = useState("")
+  const history = useHistory()
+
+  const sessionUser = useSelector(state => state.session.user);
+
+  const [name, setName] = useState('');
+  const [address, setAddress] = useState('');
+  const [city, setCity] = useState('');
+  const [state, setState] = useState('');
+  const [country, setCountry] = useState('');
+  const [previewImage, setPreviewImage] = useState('');
+  const [lat, setLat] = useState(0);
+  const [lng, setLng] = useState(0);
   const [price, setPrice] = useState(0);
+  const [description, setDescription] = useState('');
+
   const [errors, setErrors] = useState([]);
 
-  if (!sessionUser) {
-    alert("You must log in to create a spot")
-    return <Redirect to="/" />
+  const setSpot = () => {
+    setName('')
+    setAddress('')
+    setCity('')
+    setState('')
+    setCountry('')
+    setPreviewImage('')
+    setLat('')
+    setLng('')
+    setPrice('')
+    setDescription('')
+    setErrors([])
   }
- 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const validateErrors = [];
-    if (name.length < 1) validateErrors.push('Please include a title for your listing.');
-    if (city.length < 1) validateErrors.push('Please include a city.');
-    if (description.length < 1) validateErrors.push('Please include a description for your listing.');
-    if (address.length < 1 || city.length < 1 || country.length < 1 || state.length < 1) validateErrors.push('Please include a full address.');
-    if (lat.length < 1 || lng.length < 1) validateErrors.push('Please include a latitude and longitude.');
-    if (price.length < 1) {
-        validateErrors.push('Please include a price.')
-    } else if (price.length > 0) {
-        if (Number(price) === 0) {
-            validateErrors.push('Price must be more that $0')
+
+  const spotSubmit = async (e) => {
+    e.preventDefault()
+    setErrors([])
+
+    const validateSpot = []
+    if (description.length < 5) validateSpot.push('Description must be at least 5 characters')
+    if (lat < -90 || lat > 90) validateSpot.push('Latitude must be between -90 and 90.')
+    if (lng < -180 || lng > 180) validateSpot.push('Longitude must be between -180 and 180.')
+
+    setErrors(validateSpot)
+
+    if (validateSpot.length === 0) {
+        const newSpot = {
+            userId: sessionUser.id,
+            name,
+            address,
+            city,
+            state,
+            country,
+            previewImage,
+            lat,
+            lng,
+            price,
+            description,
         }
+
+        const spotId = await dispatch(spotActions.createSpot(newSpot))
+        setSpot()
+        history.push(`/spots/${spotId}`)
+
     }
-    if (validateErrors.length > 0) {
-        setErrors(validateErrors);
-        return;
-    }
+  }
 
-
-    let newSpot = {
-      name: name,
-      address: address,
-      city: city,
-      state: state,
-      country: country,
-      previewImage: previewImage,
-      lat: lat,
-      lng: lng,
-      description: description,
-      price: price,
-    };
-
-    history.push("/")
-    return dispatch(createSpot(newSpot))
-  };
+  const newName = (e) => setName(e.target.value)
+  const newAddress = (e) => setAddress(e.target.value)
+  const newCity = (e) => setCity(e.target.value)
+  const newState = (e) => setState(e.target.value)
+  const newCountry = (e) => setCountry(e.target.value)
+  const newImage = (e) => setPreviewImage(e.target.value)
+  const newLat = (e) => setLat(e.target.value)
+  const newLng = (e) => setLng(e.target.value)
+  const newDescription = (e) => setDescription(e.target.value)
+  const newPrice = (e) => setPrice(e.target.value)
 
   return (
-    <form onSubmit={handleSubmit} className='spotForm'>
+    <form className='createSpot' onSubmit={spotSubmit}>
       <ul>
-        {errors.map((error, idx) => (
-          <li key={idx}>{error}</li>
+        {errors.map((error, id) => (
+          <li key={id}>{error}</li>
         ))}
       </ul>
       <label>
-        Address:
-        <input
-          type="text"
-          placeholder="Address"
-          value={address}
-          onChange={(e) => setAddress(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        City:
-        <input
-          type="text"
-          placeholder="City"
-          value={city}
-          onChange={(e) => setCity(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        State:
-        <input
-          type="text"
-          placeholder="State"
-          value={state}
-          onChange={(e) => setState(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Country:
-        <input
-          type="text"
-          placeholder="Country"
-          value={country}
-          onChange={(e) => setCountry(e.target.value)}
-          required
-        />
-      </label>
-      <label >
-        Preview Image: 
-        <input
-          type="text"
-          value={previewImage}
-          onChange={(e) => setPreviewImage(e.target.value)}
-        />
-      </label>
-      <label>
-        Lat:
-        <input
-          type="text"
-          placeholder="Latitude"
-          value={lat}
-          onChange={(e) => setLat(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Lng:
-        <input
-          type="text"
-          placeholder="Longitude"
-          value={lng}
-          onChange={(e) => setLng(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Name:
+        Name
         <input
           type="text"
           placeholder="Name"
           value={name}
-          onChange={(e) => setName(e.target.value)}
+          onChange={newName}
           required
         />
       </label>
       <label>
-        Description:
+        Address
+        <input
+          type="text"
+          placeholder="Address"
+          value={address}
+          onChange={newAddress}
+          required
+        />
+      </label>
+      <label>
+        City
+        <input
+          type="text"
+          placeholder="City"
+          value={city}
+          onChange={newCity}
+          required
+        />
+      </label>
+      <label>
+        State
+        <input
+          type="text"
+          placeholder="State"
+          value={state}
+          onChange={newState}
+          required
+        />
+      </label>
+      <label>
+        Country
+        <input
+          type="text"
+          placeholder="Country"
+          value={country}
+          onChange={newCountry}
+          required
+        />
+      </label>
+      <label >
+        Image
+        <input
+          type="text"
+          placeholder="img-url"
+          value={previewImage}
+          onChange={newImage}
+        />
+      </label>
+      <label>
+        Latitude
+        <input
+          type="text"
+          placeholder="Latitude"
+          value={lat}
+          onChange={newLat}
+          required
+        />
+      </label>
+      <label>
+        Longitude
+        <input
+          type="text"
+          placeholder="Longitude"
+          value={lng}
+          onChange={newLng}
+          required
+        />
+      </label>
+      <label>
+        Description
         <input
           type="text"
           placeholder="Description"
           value={description}
-          onChange={(e) => setDescription(e.target.value)}
+          onChange={newDescription}
           required
         />
       </label>
       <label>
-        Price:
+        Price 
         <input
           type="text"
           value={price}
-          onChange={(e) => setPrice(e.target.value)}
+          onChange={newPrice}
           required
         />
       </label>
@@ -172,4 +192,4 @@ const CreateNewSpot = () => {
   );
 };
 
-export default CreateNewSpot;
+export default CreateSpotPage
--- frontend/src/components/SpotDetailPage/index.js
+++ frontend/src/components/SpotDetailPage/index.js
@@ -1,36 +1,32 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from "react-redux";
 import { getSpotById } from "../../store/spots";
-import "./SpotDetail.css"
-
+import "./SpotDetail.css";
 
 const SpotsDetailPage = () => {
-
-  const { id } = useParams();
-   id = Number(id);
+  const { spotId: id } = useParams();
 
   const dispatch = useDispatch();
+  const spot = useSelector(state => state.spots[id]);
 
-  const [isLoaded, setIsLoaded] = useState(false);
-  const spot = useSelector((state) => state.spots[id]);
-  
   useEffect(() => {
     dispatch(getSpotById(id));
   }, [dispatch, id]);
 
   return (
-    <div key={spot.id}>
+    <div >
       <h4 className="spotName">{spot.name}</h4>
-      <img className="spotImg" src={spot.previewImage} alt={spot.name}></img>
+      <img className="spotImgage" src={spot.previewImage} alt={spot.name}></img>
       <h3 className="spotLocation">
         {spot.city}, {spot.state}
       </h3>
       <p className="spotDescription">{spot.description}</p>
-      <p className="SpotPrice">${spot.price} night</p>
+      <p className="spotPrice">${spot.price} night</p>
+      
     </div>
   );
 };
 
 export default SpotsDetailPage;
+
