a0f759e58d00fe69a0011cfc09653e5a0cfb8b4f
JustineJang <justinejang415@gmail.com>
2022-07-27 19:39:39 -0700

    edit spot

--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -4,7 +4,6 @@ import { Redirect } from "react-router-dom";
 import * as sessionActions from "../../store/session";
 import "./SignupForm.css"
 
-
 function SignupFormPage() {
   const dispatch = useDispatch();
   const sessionUser = useSelector((state) => state.session.user);
