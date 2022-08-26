8eef9d63042c352fcda806083ab49cef07b0d297
JustineJang <justinejang415@gmail.com>
2022-08-26 15:45:14 -0700

    function

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,8 +1,8 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { NavLink } from "react-router-dom";
+
 import { getAllSpots } from "../../store/spots";
-// import { loadReviews } from "../../store/reviews";
 import PawReviews from "../ListingDetail/PawReviews";
 
 import "../CSS/HomePage1.css";
--- frontend/src/store/spots.js
+++ frontend/src/store/spots.js
@@ -50,8 +50,8 @@ const getUserSpots = (currentUserSpots) => {
 };
 
 //Get all spots
-export const getAllSpots = async (dispatch) => {
-  const response = await csrfFetch("/api/spots");
+export const getAllSpots = () => async(dispatch) => {
+  const response = await ("/api/spots");
   if (response.ok) {
     const data = await response.json();
 
