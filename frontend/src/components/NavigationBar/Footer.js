a99ca2d7be5ea02e3069780671c126bdcbddf25e
JustineJang <justinejang415@gmail.com>
2023-05-31 12:40:39 -0700

    footer

--- frontend/src/components/NavigationBar/Footer.js
+++ frontend/src/components/NavigationBar/Footer.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 import '../CSS/Footer.css'
 
 function Footer() {
   return (
-    <div className="footer-main">
-      <div className="footer-inner">
-        <div className="footer-left">Petbnb</div>
-        <div className="footer-right">
-          <span className="social-link"><a href='' target='blank'><i className="fa-brands fa-github"></i>&nbsp; Github</a></span>
-          <span className="social-link"><a href='' target='blank'><i className="fa-brands fa-linkedin"></i>&nbsp; LinkedIn</a></span>
+    <div className='footer-main'>
+      <div className='footer-container'>
+        <div className='footer-left'>© 2023 Petbnb • For Demonstration Purposes Only</div>
+        <div className='footer-right'>
+          <span className='social-link'><a href='https://github.com/jvstinejvng/airbnb_clone' target='blank'><i className='fa-brands fa-github'></i>&nbsp; Github</a></span>
+          <span className='social-link'><a href='https://www.linkedin.com/in/jvstinejvng/' target='blank'><i className='fa-brands fa-linkedin'></i>&nbsp; LinkedIn</a></span>
+          <span className='social-link'><a href='https://www.airbnb.com/' target='blank'><i className='fa-brands fa-airbnb'></i>&nbsp; Airbnb</a></span>
         </div>
       </div>
     </div>
